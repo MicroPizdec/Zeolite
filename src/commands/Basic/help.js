5cc36c98d0ec29f2eeb64925632bbc427ce5bc42
1z3ngero <thegdradio@gmail.com>
2020-07-17 21:25:26 +0700

    Revert "trying to make an embed pagination"
    This reverts commit 74dbf74120f7e9da1b821005d7ed89e264d5a84d.

--- src/commands/Basic/help.js
+++ src/commands/Basic/help.js
@@ -1,5 +1,3 @@
-const ReactionHandler = require("eris-reactions");
-
 module.exports = {
   name: "help",
   group: "BASIC_GROUP",
@@ -9,13 +7,12 @@ module.exports = {
     let embed = {
       title: client.i18n.getTranslation(language, "HELP_EMBED_TITLE"),
       color: Math.round(Math.random() * 16777216) + 1,
+      fields: [],
       footer: {
         text: "Zeolite © 2019-2020 ZariBros",
         icon_url: "https://yt3.ggpht.com/a-/AAuE7mC54pDFKe5kqwhrrNUNdwOABF0ogi8Yw4S5NZaeQQ=s288-c-k-c0xffffffff-no-rj-mo",
       },
     };
-
-    let pages = [];
 
     if (prefix == client.prefix2)
       embed.title = client.i18n.getTranslation(language, "HELP_EMBED_TITLE_OWNER_ONLY");
@@ -31,43 +28,12 @@ module.exports = {
         return `${usage} - ${client.i18n.getTranslation(language, c.description)}`;
       }).join("\n");
       if (!commandList.length) continue;
-      pages.push({
+      embed.fields.push({
         name: client.i18n.getTranslation(language, group.name),
         value: commandList,
       });
     }
 
-    let pageNumber = 0;
-    embed.fields = [ pages[0] ];
-
-    const message = await msg.channel.createMessage({ embed });
-    await message.addReaction("◀");
-    await message.addReaction("▶");
-    await message.addReaction("❌");
-
-    const reactionListener = new ReactionHandler.continuousReactionStream(
-      message,
-      (id) => id === msg.author.id,
-      false,
-      { maxMatches: 100, time: 3600000 },
-    );
-
-    reactionListener.on("reacted", async event => {
-      switch (event.emoji.name) {
-        case "◀":
-          if (pageNumber === 0) return;
-          pageNumber--;
-          break;
-        case "▶":
-          if (pageNumber === (pages.length - 1)) return;
-          pageNumber++;
-          break;
-        default: return;
-      }
-      
-      embed.fields = [ pages[pageNumber] ];
-      await message.edit({ embed });
-      await message.removeReaction(event.emoji.name, msg.author.id);
-    });
+    await msg.channel.createMessage({ embed });
   }
 };
