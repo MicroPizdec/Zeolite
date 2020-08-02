19f391126721e8df171f1172ec8a36ee35fbfbae
1z3ngero <thegdradio@gmail.com>
2020-08-02 11:11:21 +0700

    reverted server command

--- src/commands/Basic/server.js
+++ src/commands/Basic/server.js
@@ -1,6 +1,4 @@
 const strftime = require("strftime");
-const { stripIndents } = require("common-tags");
-const { CategoryChannel, TextChannel, VoiceChannel } = require("eris");
 
 module.exports = {
   name: "server",
@@ -12,8 +10,6 @@ module.exports = {
     const createdDaysAgo = Math.floor((Date.now() - msg.guild.createdAt) / (86400 * 1000));
     const joinedDaysAgo = Math.floor((Date.now() - msg.member.joinedAt) / (86400 * 1000));
 
-    const owner = await client.fetchUser(msg.guild.ownerID);
-
     const embed = {
       title: msg.channel.guild.name,
       thumbnail: { url: msg.channel.guild.iconURL },
@@ -23,22 +19,6 @@ module.exports = {
         icon_url: "https://yt3.ggpht.com/a-/AAuE7mC54pDFKe5kqwhrrNUNdwOABF0ogi8Yw4S5NZaeQQ=s288-c-k-c0xffffffff-no-rj-mo",
       },
       fields: [
-        {
-          name: _(language, "SERVERINFO_OWNER"),
-          value: owner.tag,
-          inline: true,
-        },
-        {
-          name: _(language, "SERVERINFO_CHANNELS"),
-          value: stripIndents`${_(language, "SERVERINFO_CHANNELS_CATEGORIES")} - ${msg.guild.channels.filter(c => c instanceof CategoryChannel).length}
-            ${_(language, "SERVERINFO_CHANNELS_TEXT")} - ${msg.guild.channels.filter(c => c instanceof TextChannel).length}
-            ${_(language, "SERVERINFO_CHANNELS_VOICE")} - ${msg.guild.channels.filter(c => c instanceof VoiceChannel).length}
-            ${_(language, "SERVERINFO_CHANNELS_TOTAL")} - ${msg.guild.channels.size}`,
-          inline: true,
-        },
-        {
-          name: _(language, "SERVERINFO_MEMBERS"),
-        },
         {
           name: client.i18n.getTranslation(language, "SERVERINFO_CREATION_DATE"),
           value: strftime("%e/%m/%Y, %H:%M", new Date(msg.channel.guild.createdAt)) + " " + _(language, "USERINFO_CREATED_DAYS_AGO", createdDaysAgo),
@@ -50,6 +30,26 @@ module.exports = {
         {
           name: client.i18n.getTranslation(language, "SERVERINFO_DATE_OF_YOUR_JOIN"),
           value: strftime("%e/%m/%Y, %H:%M", new Date(msg.member.joinedAt)) + " " + _(language, "USERINFO_CREATED_DAYS_AGO", joinedDaysAgo),
+        },
+        {
+          name: client.i18n.getTranslation(language, "SERVERINFO_CHANNELS"),
+          value: msg.channel.guild.channels.size,
+        },
+        {
+          name: client.i18n.getTranslation(language, "SERVERINFO_MEMBERS"),
+          value: msg.channel.guild.members.size,
+        },
+        {
+          name: client.i18n.getTranslation(language, "SERVERINFO_BOTS"),
+          value: msg.channel.guild.members.filter(m => m.bot).length,
+        },
+        {
+          name: client.i18n.getTranslation(language, "SERVERINFO_ROLES"),
+          value: msg.channel.guild.roles.size,
+        },
+        {
+          name: client.i18n.getTranslation(language, "SERVERINFO_REGION"),
+          value: msg.channel.guild.region,
         },
       ],
     };
