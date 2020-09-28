8fda7e34ad048e648eedbbcd0e81e29067249a0b
1z3ngero <1z3ngero@gmail.com>
2020-09-28 23:15:48 +0700

    help command overhaul

--- src/commands/Basic/help.js
+++ src/commands/Basic/help.js
@@ -1,5 +1,3 @@
-const ReactionHandler = require("../../reactionHandler");
-
 module.exports = {
   name: "help",
   group: "BASIC_GROUP",
@@ -9,6 +7,7 @@ module.exports = {
   aliases: [ "h" ],
   async run(client, msg, args, prefix, lang) {
     let embed = {
+      fields: [],
       color: Math.round(Math.random() * 16777216) + 1,
       footer: {
         text: "Zeolite © 2019-2020 ZariBros",
@@ -19,7 +18,8 @@ module.exports = {
     const cmdName = args[0];
     
     if (!cmdName) {
-      let pages = [];
+      embed.title = t(lang, "HELP_EMBED_TITLE");
+      embed.description = t(lang, "HELP_EMBED_DESC", prefix);
 
       if (prefix == client.prefix2)
         embed.title = client.i18n.getTranslation(lang, "HELP_EMBED_TITLE_OWNER_ONLY");
@@ -27,54 +27,18 @@ module.exports = {
       for (const group of client.groups.values()) {
         let filterFunction = c => !c.ownerOnly;
         if (prefix == client.prefix2) filterFunction = c => c.ownerOnly;
-        const commandList = group.commands.filter(filterFunction).map(c => {
-          let usage = `\`${prefix}${c.name}`;
-          if (c.usage) usage += ` ${client.i18n.getTranslation(lang, c.usage)}\``;
-          else usage += "`";
 
-          return `${usage} - ${client.i18n.getTranslation(lang, c.description)}`;
-        }).join("\n");
-        if (!commandList.length) continue;
-        pages.push({
-          name: client.i18n.getTranslation(lang, group.name),
-          value: commandList,
+        const cmdList = group.commands.filter(filterFunction)
+          .map(c => `\`${c.name}\``).join(", ");
+        if (!cmdList) continue;
+
+        embed.fields.push({
+          name: t(lang, group.name),
+          value: cmdList,
         });
       }
 
-      let pageNumber = 0;
-      embed.fields = [ pages[0] ];
-
-      embed.title = _(lang, "HELP_EMBED_TITLE", pageNumber + 1, pages.length);
-
       const message = await msg.channel.createMessage({ embed });
-      await message.addReaction("◀");
-      await message.addReaction("▶");
-      // await message.addReaction("❌");
-
-      const reactionListener = new ReactionHandler(
-        message,
-        (id) => id === msg.author.id,
-        3600000,
-      );
-
-      reactionListener.on("reaction", async (msg, emoji) => {
-        switch (emoji.name) {
-          case "◀":
-            if (pageNumber === 0) return;
-            pageNumber--;
-            break;
-          case "▶":
-            if (pageNumber === (pages.length - 1)) return;
-            pageNumber++;
-            break;
-          default: return;
-        }
-
-        embed.title = _(lang, "HELP_EMBED_TITLE", pageNumber + 1, pages.length);
-        embed.fields = [ pages[pageNumber] ];
-        await message.edit({ embed });
-        // await message.removeReaction(event.emoji.name, msg.author.id);
-      });
     } else {
       const cmd = client.commands.find(c => c.name === cmdName || (c.aliases && c.aliases.includes(cmdName)));
 
