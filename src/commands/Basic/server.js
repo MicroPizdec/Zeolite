165a3f56f086b4a0bfd4406787091531fe55ca8e
1z3ngero <1z3ngero@gmail.com>
2020-10-03 17:04:54 +0700

    z/server overhaul

--- src/commands/Basic/server.js
+++ src/commands/Basic/server.js
@@ -1,4 +1,6 @@
 const strftime = require("strftime");
+const { CategoryChannel, TextChannel, VoiceChannel } = require("eris");
+const { stripIndents } = require("common-tags");
 
 module.exports = {
   name: "server",
@@ -6,50 +8,53 @@ module.exports = {
   description: "SERVERINFO_COMMAND_DESCRIPTION",
   guildOnly: true,
   aliases: [ "s", "serverinfo" ],
-  async run(client, msg, args, prefix, language) {
+  async run(client, msg, args, prefix, lang) {
     const createdDaysAgo = Math.floor((Date.now() - msg.guild.createdAt) / (86400 * 1000));
     const joinedDaysAgo = Math.floor((Date.now() - msg.member.joinedAt) / (86400 * 1000));
 
     const embed = {
-      title: msg.channel.guild.name,
-      thumbnail: { url: msg.channel.guild.iconURL },
+      author: {
+        name: msg.guild.name,
+        icon_url: msg.guild.iconURL,
+      },
       color: Math.round(Math.random() * 16777216) + 1,
       footer: {
-        text: "Zeolite © 2019-2020 ZariBros",
-        icon_url: "https://yt3.ggpht.com/a-/AAuE7mC54pDFKe5kqwhrrNUNdwOABF0ogi8Yw4S5NZaeQQ=s288-c-k-c0xffffffff-no-rj-mo",
+        text: `${client.user.username} © 2019-2020 ZariBros`,
+        icon_url: client.user.avatarURL,
       },
       fields: [
         {
-          name: client.i18n.getTranslation(language, "SERVERINFO_CREATION_DATE"),
-          value: strftime("%e/%m/%Y, %H:%M", new Date(msg.channel.guild.createdAt)) + " " + _(language, "USERINFO_CREATED_DAYS_AGO", createdDaysAgo),
-        },
-        {
           name: "ID",
-          value: msg.channel.guild.id,
+          value: msg.guild.id,
         },
         {
-          name: client.i18n.getTranslation(language, "SERVERINFO_DATE_OF_YOUR_JOIN"),
-          value: strftime("%e/%m/%Y, %H:%M", new Date(msg.member.joinedAt)) + " " + _(language, "USERINFO_CREATED_DAYS_AGO", joinedDaysAgo),
+          name: t(lang, "SERVERINFO_CREATION_DATE"),
+          value: strftime("%e %b %Y, %H:%M", new Date(msg.guild.createdAt)) + " " + _(lang, "USERINFO_CREATED_DAYS_AGO", createdDaysAgo),
         },
         {
-          name: client.i18n.getTranslation(language, "SERVERINFO_CHANNELS"),
-          value: msg.channel.guild.channels.size,
+          name: t(lang, "SERVERINFO_CHANNELS"),
+          value: stripIndents`${t(lang, "SERVERINFO_TEXT")} - ${msg.guild.channels.filter(c => c instanceof TextChannel).length}
+          ${t(lang, "SERVERINFO_VOICE")} - ${msg.guild.channels.filter(c => c instanceof VoiceChannel).length}
+          ${t(lang, "SERVERINFO_CATEGORIES")} - ${msg.guild.channels.filter(c => c instanceof CategoryChannel).length}`,
+          inline: true,
         },
         {
-          name: client.i18n.getTranslation(language, "SERVERINFO_MEMBERS"),
-          value: msg.channel.guild.members.size,
+          name: t(lang, "SERVERINFO_MEMBERS"),
+          value: stripIndents`${t(lang, "SERVERINFO_MEMBERS_ONLINE")} - ${msg.guild.members.filter(m => m.status == "online").length}
+          ${t(lang, "SERVERINFO_MEMBERS_IDLE")} - ${msg.guild.members.filter(m => m.status == "idle").length}
+          ${t(lang, "SERVERINFO_MEMBERS_DND")} - ${msg.guild.members.filter(m => m.status == "dnd").length}
+          ${t(lang, "SERVERINFO_MEMBERS_OFFLINE")} - ${msg.guild.members.filter(m => m.status == "offline").length}
+          ${t(lang, "SERVERINFO_MEMBERS_BOTS")} - ${msg.guild.members.filter(m => m.bot).length}
+          ${t(lang, "SERVERINFO_MEMBERS_TOTAL")} - ${msg.guild.memberCount}`,
+          inline: true,
         },
         {
-          name: client.i18n.getTranslation(language, "SERVERINFO_BOTS"),
-          value: msg.channel.guild.members.filter(m => m.bot).length,
+          name: t(lang, "SERVERINFO_ROLES"),
+          value: msg.guild.roles.size,
         },
         {
-          name: client.i18n.getTranslation(language, "SERVERINFO_ROLES"),
-          value: msg.channel.guild.roles.size,
-        },
-        {
-          name: client.i18n.getTranslation(language, "SERVERINFO_REGION"),
-          value: msg.channel.guild.region,
+          name: t(lang, "SERVERINFO_REGION"),
+          value: msg.guild.region,
         },
       ],
     };
