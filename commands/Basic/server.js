b37a6404df1e09c12c1cad9f8ae2bfb3261b3535
1z3ngero <1z3ngero@gmail.com>
2020-11-13 01:06:49 +0700

    showing server by id

--- commands/Basic/server.js
+++ commands/Basic/server.js
@@ -9,13 +9,16 @@ module.exports = {
   guildOnly: true,
   aliases: [ "s", "serverinfo" ],
   async run(client, msg, args, prefix, lang) {
-    const createdDaysAgo = Math.floor((Date.now() - msg.guild.createdAt) / (86400 * 1000));
+    const guild = client.owners.includes(msg.author.id) ?
+      client.guilds.get(args[0]) || msg.guild : msg.guild;
+
+    const createdDaysAgo = Math.floor((Date.now() - guild.createdAt) / (86400 * 1000));
     const joinedDaysAgo = Math.floor((Date.now() - msg.member.joinedAt) / (86400 * 1000));
 
     const embed = {
       author: {
-        name: msg.guild.name,
-        icon_url: msg.guild.iconURL,
+        name: guild.name,
+        icon_url: guild.iconURL,
       },
       color: Math.round(Math.random() * 16777216) + 1,
       footer: {
@@ -25,39 +28,39 @@ module.exports = {
       fields: [
         {
           name: "ID",
-          value: msg.guild.id,
+          value: guild.id,
         },
         {
           name: t(lang, "SERVERINFO_CREATION_DATE"),
-          value: strftime("%e %b %Y, %H:%M", new Date(msg.guild.createdAt)) + " " + _(lang, "USERINFO_CREATED_DAYS_AGO", createdDaysAgo),
+          value: strftime("%e %b %Y, %H:%M", new Date(guild.createdAt)) + " " + _(lang, "USERINFO_CREATED_DAYS_AGO", createdDaysAgo),
         },
         {
           name: t(lang, "SERVERINFO_CHANNELS"),
-          value: stripIndents`${t(lang, "SERVERINFO_TEXT")} - ${msg.guild.channels.filter(c => c instanceof TextChannel).length}
-          ${t(lang, "SERVERINFO_VOICE")} - ${msg.guild.channels.filter(c => c instanceof VoiceChannel).length}
-          ${t(lang, "SERVERINFO_CATEGORIES")} - ${msg.guild.channels.filter(c => c instanceof CategoryChannel).length}`,
+          value: stripIndents`${t(lang, "SERVERINFO_TEXT")} - ${guild.channels.filter(c => c instanceof TextChannel).length}
+          ${t(lang, "SERVERINFO_VOICE")} - ${guild.channels.filter(c => c instanceof VoiceChannel).length}
+          ${t(lang, "SERVERINFO_CATEGORIES")} - ${guild.channels.filter(c => c instanceof CategoryChannel).length}`,
           inline: true,
         },
         {
           name: t(lang, "SERVERINFO_MEMBERS"),
-          value: stripIndents`${t(lang, "SERVERINFO_MEMBERS_BOTS")} - ${msg.guild.members.filter(m => m.bot).length}
-          ${t(lang, "SERVERINFO_MEMBERS_TOTAL")} - ${msg.guild.memberCount}`,
+          value: stripIndents`${t(lang, "SERVERINFO_MEMBERS_BOTS")} - ${guild.members.filter(m => m.bot).length}
+          ${t(lang, "SERVERINFO_MEMBERS_TOTAL")} - ${guild.memberCount}`,
           inline: true,
         },
         {
           name: t(lang, "SERVERINFO_EMOJIS"),
-          value: stripIndents`${t(lang, "SERVERINFO_EMOJIS_STATIC")} - ${msg.guild.emojis.filter(e => !e.animated).length}
-            ${t(lang, "SERVERINFO_EMOJIS_ANIMATED")} - ${msg.guild.emojis.filter(e => e.animated).length}
-            ${t(lang, "SERVERINFO_EMOJIS_TOTAL")} - ${msg.guild.emojis.length}`,
+          value: stripIndents`${t(lang, "SERVERINFO_EMOJIS_STATIC")} - ${guild.emojis.filter(e => !e.animated).length}
+            ${t(lang, "SERVERINFO_EMOJIS_ANIMATED")} - ${guild.emojis.filter(e => e.animated).length}
+            ${t(lang, "SERVERINFO_EMOJIS_TOTAL")} - ${guild.emojis.length}`,
           inline: true,
         },
         {
           name: t(lang, "SERVERINFO_ROLES"),
-          value: msg.guild.roles.size,
+          value: guild.roles.size,
         },
         {
           name: t(lang, "SERVERINFO_REGION"),
-          value: msg.guild.region,
+          value: guild.region,
         },
       ],
     };
