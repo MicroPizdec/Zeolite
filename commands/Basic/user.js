93527f6bb36e5c308682b849f3246b4d1f84a09e
1z3ngero <1z3ngero@gmail.com>
2020-11-28 15:52:38 +0700

    .

--- commands/Basic/user.js
+++ commands/Basic/user.js
@@ -17,7 +17,7 @@ module.exports = {
         m.nick && m.nick.toLowerCase().startsWith(userID.toLowerCase()) ||
         m.tag.toLowerCase().startsWith(userID.toLowerCase()) ||
         m.id == userID
-      );
+      ) || await client.fetchUser(userID);
     
     if (!member) return msg.channel.createMessage(t(language, "USER_NOT_FOUND"));
 
@@ -25,8 +25,9 @@ module.exports = {
 
     const createdDaysAgo = Math.floor((Date.now() - member.createdAt) / (1000 * 86400));
     const joinedDaysAgo = Math.floor((Date.now() - member.joinedAt) / (1000 * 86400));
-    
-    const roleList = Array.from(member.roleObjects.values())
+
+    let roleList;
+    if (member.roleObjects) roleList = Array.from(member.roleObjects.values())
       .sort((a, b) => b.position - a.position)
       .map(r => r.mention)
       .join(", ");
@@ -63,17 +64,8 @@ module.exports = {
           inline: true,
         },
         {
-          name: t(language, "USERINFO_JOINDATE"),
-          value: strftime("%e %b %Y, %H:%M", new Date(member.joinedAt)) + " " + _(language, "USERINFO_CREATED_DAYS_AGO", joinedDaysAgo),
-          inline: true,
-        },
-        {
           name: t(language, "USERINFO_BOT_TITLE"),
           value: t(language, "USERINFO_BOT_DEFINE", member.bot),
-        },
-        {
-          name: t(language, "USERINFO_ROLES"),
-          value: roleList,
         },
         {
           name: t(language, "USERINFO_ZETCOINS_TITLE"),
@@ -81,59 +73,24 @@ module.exports = {
         },
         {
           name: t(language, "USERINFO_ZETCOINS_TOP"),
-          value: t(language, "USERINFO_ZETCOINS_TOP_POS", globalTopPos, topPos),
+          value: t(language, "USERINFO_ZETCOINS_TOP_POS", globalTopPos, topPos || "n/a"),
         },
       ],
     };
-    
-    if (member.game) {
-      if (!member.game.name) {
-        return msg.channel.createMessage({ embed });
-      }
 
-      let emoji;
-      if (member.game.emoji) {
-        let animated = member.game.emoji.animated ? "a" : "";
-        if (member.game.emoji.id) {
-          emoji = `<${animated}:${member.game.emoji.name}:${member.game.emoji.id}>`;
-        } else {
-          emoji = member.game.emoji.name;
-        }
-      }
-  
-      switch (member.game.type) {
-        case 4:
-          embed.fields.unshift({
-            name: t(language, "USERINFO_CUSTOM_STATUS"),
-            value: emoji ? `${emoji} ${member.game.state || ""}` : member.game.state,
-          });
-          break;
-        case 3:
-          embed.fields.unshift({
-            name: t(language, "USERINFO_WATCHING"),
-            value: member.game.name,
-          });
-          break;
-        case 2:
-          embed.fields.unshift({
-            name: t(language, "USERINFO_LISTENING"),
-            value: member.game.name,
-          });
-          break;
-        case 1:
-          embed.fields.unshift({
-            name: t(language, "USERINFO_STREAMING"),
-            value: `[${member.game.name}](${member.game.url})`,
-          });
-          break;
-        case 0:
-          embed.fields.unshift({
-            name: t(language, "USERINFO_PLAYING"),
-            value: member.game.name,
-          });
-          break;
-      }
+    let indexInc = 0;
+    if (member.joinedAt) {
+      embed.fields.splice(2, 0, {
+        name: t(language, "USERINFO_JOINDATE"),
+        value: strftime("%e %b %Y, %H:%M", new Date(member.joinedAt)) + " " + _(language, "USERINFO_CREATED_DAYS_AGO", joinedDaysAgo)
+      });
+      indexInc++;
     }
+
+    if (roleList) embed.fields.splice(3 + indexInc, 0, {
+      name: t(language, "USERINFO_ROLES"),
+      value: roleList,
+    });
 
     await msg.channel.createMessage({ embed });
   }
