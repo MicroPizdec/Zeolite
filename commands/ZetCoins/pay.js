966f794ce0cf8ba3aeb4588f642b155f1849bf39
1z3ngero <1z3ngero@gmail.com>
2021-01-12 13:37:41 +0700

    rewrote pay command

---
+++ commands/ZetCoins/pay.js
@@ -0,0 +1,96 @@
+const { ZETCOINS_GROUP } = require("../../locales/en");
+
+module.exports = {
+  name: "pay",
+  group: "ZETCOINS_GROUP",
+  description: "SENDCOINS_COMMAND_DESCRIPTION",
+  usage: "SENDCOINS_COMMAND_USAGE",
+  aliases: [ "sendcoins" ],
+  argsRequired: true,
+  async run(client, msg, args, prefix, lang) {
+    let [ userID, sum ] = args;
+
+    const user = msg.mentions.length ? msg.mentions[0] :
+      client.users.find(u => u.tag == userID || u.id == userID);
+    
+    if (!user) {
+      return msg.channel.createMessage(t(lang, "INVALID_USER_PROVIDED"));
+    }
+
+    if (user.id == msg.author.id) {
+      return msg.channel.createMessage(t(lang, "CANNOT_SEND_COINS_TO_SELF"));
+    }
+    if (user.bot) {
+      return msg.channel.createMessage(t(lang, "CANNOT_SEND_COINS_TO_BOT"));
+    }
+
+    if (!sum || sum < 0) {
+      return msg.channel.createMessage(t(lang, "SENDCOINS_NO_AMOUNT"));
+    }
+
+    if (isNaN(sum)) {
+      return msg.channel.createMessage(t(lang, "SENDCOINS_AMOUNT_IS_NAN"));
+    }
+
+    sum = Math.round(sum);
+
+    const authorBalance = await zetCoins.findOrCreate({ where: { user: msg.author.id } })
+      .then(b => b[0]);
+    const userBalance = await zetCoins.findOrCreate({ where: { user: user.id } })
+      .then(b => b[0]);
+    
+    if (authorBalance.balance < sum) {
+      return msg.channel.createMessage(t(lang, "SENDCOINS_NOT_ENOUGH_MONEY", authorBalance.balance));
+    }
+
+    const confirmEmbed = {
+      title: t(lang, "SENDCOINS_CONFIRMATION_TITLE", sum, user),
+      description: t(lang, "SENDCOINS_CONFIRMATION_DESCRIPTION"),
+      color: await msg.author.embedColor(),
+    };
+
+    const message = await msg.channel.createMessage({ embed: confirmEmbed });
+    
+    const response = await msg.channel.awaitMessages(
+      m => m.author == msg.author && ((m.content == "y" || m.content == "yes") || (m.content == "n" || m.content == "no")),
+      { time: 30000, maxMatches: 1 }
+    );
+    if (!response.length) {
+      const embed = {
+        author: {
+          name: msg.author.tag,
+          icon_url: msg.author.avatarURL,
+        },
+        description: t(lang, "SENDCOINS_TIME_EXPIRED"),
+        color: await msg.author.embedColor(),
+      };
+
+      return message.edit({ embed });
+    } else if (response[0].content == "y" || response[0].content == "yes") {
+      await userBalance.update({ balance: userBalance.balance + sum });
+      await authorBalance.update({ balance: authorBalance.balance - sum });
+
+      const embed = {
+        author: {
+          name: msg.author.tag,
+          icon_url: msg.author.avatarURL,
+        },
+        description: t(lang, "SENDCOINS_SUCCESSFULLY_SENT", sum, user),
+        color: await msg.author.embedColor(),
+      };
+
+      return message.edit({ embed });
+    } else {
+      const embed = {
+        author: {
+          name: msg.author.tag,
+          icon_url: msg.author.avatarURL,
+        },
+        description: t(lang, "SENDCOINS_CANCELLED_TRANSACTION"),
+        color: await msg.author.embedColor(),
+      };
+
+      return message.edit({ embed });
+    }
+  }
+}
