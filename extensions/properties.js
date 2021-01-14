90ea84067923cd0b6ae46545ed1b3badc36f56b3
1z3ngero <1z3ngero@gmail.com>
2021-01-14 23:44:35 +0700

    getting default embed color from config

--- extensions/properties.js
+++ extensions/properties.js
@@ -3,6 +3,6 @@ const { User } = require("eris");
 module.exports.load = () => {
   User.prototype.embedColor = function embedColor() {
     return embColors.findOne({ where: { user: this.id } })
-      .then(c => c?.color || 0x9f00ff);
+      .then(c => c?.color || config.embedColor);
   }
 }
