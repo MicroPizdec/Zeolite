6753aa83a1084484dcb986741c47d86103f24c4a
ZariBros <mr.rybka06@gmail.com>
2021-01-27 17:42:20 +0300

    some changes on avatar cmd

--- commands/Basic/avatar.js
+++ commands/Basic/avatar.js
@@ -15,12 +15,50 @@ module.exports = {
       const guild = client.owners.includes(msg.author.id) ?
         client.guilds.get(args[1]) || msg.guild : msg.guild;
 
+      if(!guild.icon) return await msg.channel.createMessage(t(lang, "NO_ICON"))
+
       const embed = {
         author: {
           name: t(lang, "SERVER_ICON", guild.name),
           url: guild.iconURL,
         },
         image: { url: guild.iconURL },
+        color: await msg.author.embedColor(),
+      };
+
+      return msg.channel.createMessage({ embed });
+    } 
+
+    if (args[0] === "-sp" || args[0] == "splash") {
+      const guild = client.owners.includes(msg.author.id) ?
+        client.guilds.get(args[1]) || msg.guild : msg.guild;
+      
+      if(!guild.splash) return await msg.channel.createMessage(t(lang, "NO_SPLASH"))
+
+      const embed = {
+        author: {
+          name: t(lang, "SERVER_SPLASH", guild.name),
+          url: guild.dynamicSplashURL("png", 4096),
+        },
+        image: { url: guild.dynamicSplashURL("png", 4096) },
+        color: await msg.author.embedColor(),
+      };
+
+      return msg.channel.createMessage({ embed });
+    } 
+
+    if (args[0] === "-b" || args[0] == "banner") {
+      const guild = client.owners.includes(msg.author.id) ?
+        client.guilds.get(args[1]) || msg.guild : msg.guild;
+      
+      if(!guild.banner) return await msg.channel.createMessage(t(lang, "NO_BANNER"))
+
+      const embed = {
+        author: {
+          name: t(lang, "SERVER_BANNER", guild.name),
+          url: guild.dynamicBannerURL("png", 4096),
+        },
+        image: { url: guild.dynamicBannerURL("png", 4096) },
         color: await msg.author.embedColor(),
       };
 
