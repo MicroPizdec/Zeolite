4ae291ee8e41aa097e499075532f3df3ac7d1ea4
1z3ngero <1z3ngero@gmail.com>
2021-01-30 16:17:53 +0700

    removed support and notes commands

--- dbModels/notes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("notes", {
-    name: {
-      type: DataTypes.TEXT,
-    },
-    content: DataTypes.TEXT,
-    author: DataTypes.STRING,
-  }, { timestamps: false });
-}
