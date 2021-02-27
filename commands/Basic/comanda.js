01afb1d753e7830a403b30bfdb3714dd6d99c99d
ZariBros <mr.rybka06@gmail.com>
2021-02-27 17:01:42 +0300

    easter egg

---
+++ commands/Basic/comanda.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "[команда]",
+    group: "BASIC_GROUP",
+    description: "команда",
+    hidden: true,
+async run(client, msg, args){
+    return msg.channel.createMessage("<:commandblock:798220658818416680>")
+}
+}
