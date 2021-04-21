d1dcac7c6749abf3fd9e265932862d4811790748
ZariBros <mr.rybka06@gmail.com>
2021-04-21 14:46:53 +0300

    Added config example (for convenience)

---
+++ config_example.js
@@ -0,0 +1,17 @@
+module.exports = {
+    prefix: "your prefix",
+    token: "your-token",
+    owners: [ "id" ],
+    webhookID: "id",
+    webhookToken: "webhook-token",
+    supportChannelID: 'id',
+    embedColor: 0x9f00ff,
+    db: {
+      dialect: "sqlite",
+      storage: "bot.db",
+    },
+    deviantart: {
+      clientID: "your deviantart app's client id",
+      clientSecret: "your deviantart app's client secret",
+    }
+  }
