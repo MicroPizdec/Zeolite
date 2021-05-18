cf5992130450e3bf7ebf149d2f77aa4b9579b356
1z3ngero <1z3ngero@gmail.com>
2021-05-18 18:55:43 +0700

    what tf is that codestyle

--- commands/NSFW/hentai.js
+++ commands/NSFW/hentai.js
@@ -2,30 +2,29 @@ const NSFW = require("discord-nsfw");
 const nsfw = new NSFW();
 
 module.exports = {
-    name: "hentai",
-    group: "NSFW_GROUP",
-    description: "HENTAI_DESCRIPTION",
-    aliases: [ "hn" ],
-    async run(client, msg, args, prefix, lang) {
-        
-        if (!msg.channel.nsfw) {
-            return msg.reply(t(lang, "NOT_NSFW_CHANNEL", this.name));
-        }
-        
-        const nsfwimage = await nsfw.hentai();
-          
-        const embed = {
-            author: {
-                name: msg.author.tag,
-                icon_url: msg.author.avatarURL,
-            },
-            image: { url: nsfwimage },
-            color: await msg.author.embedColor(),
-            footer: {
-                text: `${client.user.username} © ZariBros`,
-                icon_url: client.user.avatarURL,
-            },
-        }
-        await msg.reply({ embed });
+  name: "hentai",
+  group: "NSFW_GROUP",
+  description: "HENTAI_DESCRIPTION",
+  aliases: [ "hn" ],
+  async run(client, msg, args, prefix, lang) {
+    if (!msg.channel.nsfw) {
+      return msg.reply(t(lang, "NOT_NSFW_CHANNEL", this.name));
     }
+        
+    const nsfwimage = await nsfw.hentai();
+          
+    const embed = {
+      author: {
+        name: msg.author.tag,
+        icon_url: msg.author.avatarURL,
+      },
+      image: { url: nsfwimage },
+      color: await msg.author.embedColor(),
+      footer: {
+        text: `${client.user.username} © ZariBros`,
+        icon_url: client.user.avatarURL,
+      },
+    }
+    await msg.reply({ embed });
+  }
 }
