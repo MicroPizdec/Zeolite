a22e67b08e2cbcdd98c3b6a0c091d0c2d5efe7b1
1z3ngero <1z3ngero@gmail.com>
2021-06-25 21:12:53 +0700

    ты свин бомж

--- extensions/database.js
+++ extensions/database.js
@@ -15,12 +15,10 @@ module.exports.load = async client => {
 
   global.db = initDB(sequelize, Sequelize.DataTypes);
 
-  client.once("ready", async () => {
-    await sequelize.sync()
-      .then(() => databaseLogger.info("Connected successfully to the database."))
-      .catch(err => databaseLogger.error(`Failed to connect to the database:\n${err.stack}`));
-  });
-
+  await sequelize.sync()
+    .then(() => databaseLogger.info("Connected successfully to the database."))
+    .catch(err => databaseLogger.error(`Failed to connect to the database:\n${err.stack}`));
+    
   client.on("guildDelete", async guild => {
     await warns.destroy({ where: { server: guild.id } });
     await prefixes.destroy({ where: { server: guild.id } });
