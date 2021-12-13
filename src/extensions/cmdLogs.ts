60ab900b37eafc906a3238ff9d0dc7aea5947acc
1z3ngero <1z3ngero@gmail.com>
2021-12-13 17:32:35 +0700

    showing command args in logs

--- src/extensions/cmdLogs.ts
+++ src/extensions/cmdLogs.ts
@@ -10,8 +10,11 @@ export default class CmdLogsExtension extends ZeoliteExtension {
   webhook: WebhookClient;
 
   async onCommandSuccess(ctx: ZeoliteContext) {
+    const options = ctx.interaction.options.data.map(d => `${d.name}: ${d.value}`);
+
     const embed = new MessageEmbed()
       .setTitle(`Command \`${ctx.commandName}\` used`)
+      .setDescription(`/${ctx.commandName} ${options.join(" ")}`)
       .addField("User", `${ctx.user.tag} (ID: ${ctx.user.id})`)
       .addField("Channel", `${(ctx.channel as GuildChannel)?.name} (ID: ${ctx.channel?.id})`)
       .addField("Guild", `${ctx.guild?.name} (ID: ${ctx.guild?.id})`);
@@ -28,8 +31,11 @@ export default class CmdLogsExtension extends ZeoliteExtension {
     
     await ctx.reply({ embeds: [ errEmbed ], ephemeral: true });
 
+    const options = ctx.interaction.options.data.map(d => `${d.name}: ${d.value}`);
+
     const embed = new MessageEmbed()
       .setTitle(`:x: An error occurred while executing command \`${ctx.commandName}\``)
+      .setDescription(`/${ctx.commandName} ${options.join(" ")}`)
       .setColor("RED")
       .addField("Error", `\`\`\`${error}\`\`\``)
       .addField("User", `${ctx.user.tag} (ID: ${ctx.user.id})`)
