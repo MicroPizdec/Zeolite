3672ba67f7ed15db5123d04aabc9915defc414ce
1z3ngero <1z3ngero@gmail.com>
2021-12-13 14:39:42 +0700

    forgot to add strings for stats command

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -2,6 +2,16 @@ export default {
   langName: "en",
   testString: "Test string",
   notBotOwner: "> :x: You aren't the bot owner!",
+  commandError: ":x: An error occurred while running the command.",
+  commandErrorDesc: "This error was sent to the bot developers and will be fixed as soon as possible.",
 
   avatarTitle: "Avatar of %s",
+
+  stats: "Stats",
+  statsUptime: "Uptime",
+  statsRamUsed: "RAM used",
+  statsServers: "Servers",
+  statsUsers: "Users",
+  statsChannels: "Channels",
+  statsCpu: "CPU",
 }
