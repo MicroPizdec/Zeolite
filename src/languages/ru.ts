1e95ff5070071c2b279f51296063abd0135fffba
alyzter <mr.rybka06@gmail.com>
2021-12-14 22:29:50 +0300

    Just in case

---
+++ src/languages/ru.ts
@@ -0,0 +1,33 @@
+/* export default {
+    langName: "ru",
+    testString: "Тестовая строка",
+    notBotOwner: "> :x: Вы не являетесь владельцем бота!",
+    commandError: ":x: Произошла ошибка при выполнении команды.",
+    commandErrorDesc: "Эта ошибка была выслана разработчикам бота, и будет исправлена в ближайшее время.",
+  
+    avatarTitle: "Аватар %s",
+  
+    stats: "Статистика",
+    statsUptime: "Аптайм",
+    statsRamUsed: "Использовано ОЗУ",
+    statsPing: "Пинг",
+    statsServers: "Серверов",
+    statsUsers: "Пользователей",
+    statsChannels: "Каналов",
+    statsCpu: "Процессор",
+  
+    userBadges: "Значки",
+    userBadgesNone: "Нет",
+    userFooter: `ID: %s\nЗарегистрировался %d дней назад`,
+    userJoinDate: "Пришёл на сервер",
+    userRoles: "Роли",
+  
+    infoTitle: "О боте",
+    infoDesc: "**Пусть ваше путешествие начнётся.**\nПростой в плане экономики, модерации, и много чего многофункциональный бот написанный на TypeScript с библиотекой discord.js.",
+    infoLinks: "Ссылки",
+    infoInvite: "Добавить меня на ваш сервер",
+    infoSupportServer: "Сервер поддержки",
+    infoRepository: "Репозиторий GitHub",
+    infoDonate: "Пожертвовать",
+  }
+*/
