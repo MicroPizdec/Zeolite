70a4ff1cbf60c27000702c7b9e0146283d031796
1z3ngero <1z3ngero@gmail.com>
2021-12-15 00:05:13 +0700

    added config example (hello alyzter)

---
+++ src/config.example.ts
@@ -0,0 +1,6 @@
+export default {
+  token: "put your bot token here",
+  owners: [ "put your user id here" ],
+  dbUri: "sqlite:bot.db", // or use another
+  webhookUrl: "put your cmd logs webhook url here",
+}
