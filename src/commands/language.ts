2786d060a204d6223cad48cfeb1907dbfe64a4e9
1z3ngero <1z3ngero@gmail.com>
2022-01-19 23:11:51 +0700

    added subcommands to language command

--- src/commands/language.ts
+++ src/commands/language.ts
@@ -8,18 +8,31 @@ export default class LanguageCommand extends ZeoliteCommand {
   description = "Changes your language or shows a list of them";
   options = [
     {
-      type: 3,
-      name: "lang",
-      description: "Language",
-      required: false,
-    }
+      type: 1,
+      name: "get",
+      description: "Shows list of available languages and your current language",
+    },
+    {
+      type: 1,
+      name: "set",
+      description: "Changes your language",
+      options: [
+        {
+          type: 3,
+          name: "lang",
+          description: "Language",
+          required: true,
+        },
+      ],
+    },
   ];
 
   async run(ctx: ZeoliteContext) {
-    let language = ctx.interaction.options.getString("lang");
-    let dbLang = await Languages.findOne({ where: { userID: ctx.user.id } });
+    const subcommand = ctx.options.getSubcommand();
+
+    const dbLang = await Languages.findOne({ where: { userID: ctx.user.id } });
     
-    if (!language) {
+    if (subcommand == "get") {
       const availableLangs = Object.keys(this.client.localization.languageStrings);
 
       const embed = new MessageEmbed()
@@ -31,6 +44,8 @@ export default class LanguageCommand extends ZeoliteCommand {
       
       await ctx.reply({ embeds: [ embed ], ephemeral: true });
     } else {
+      const language = ctx.options.getString("lang", true);
+      
       if (!["en", "ru"].includes(language)) {
         await ctx.reply({ content: ctx.t("langInvalid"), ephemeral: true });
       }
